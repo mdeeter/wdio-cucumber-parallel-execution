19d80ee03214642ac93152e643c6883bfbda4895
Michael Deeter <mdeeter@users.noreply.github.com>
2025-12-11 07:49:59 -0600

    build to a dist folder

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var getConsolidatedData = require('./getConsolidatedData');
-var performSetup = require('./performSetup');
-
-module.exports = {
-    getConsolidatedData: getConsolidatedData,
-    performSetup: performSetup
-};
